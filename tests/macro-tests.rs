c92f68aaec0e2875d7dea015c49d59cd7e2b5dc3
Jose Acevedo <acevedoflores.jose@gmail.com>
2021-11-19 21:13:22 -0800

    refactor: rename the node tests folder

--- tests/macro-tests.rs
+++ tests/macro-tests.rs
@@ -6,15 +6,16 @@ mod utils;
 #[test]
 fn try_build() {
     let t = trybuild::TestCases::new();
-    t.pass("derived_class/src/derived_class.rs");
+    t.pass("node_tests/src/derived_class.rs");
+
     let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
-    let n = manifest_dir.join("derived_class");
+    let path_to_node_tests = manifest_dir.join("node_tests");
     let npm_path = utils::locate_npm();
     let mut cmd = Command::new(npm_path);
 
     cmd.arg("test")
-        .current_dir(n)
+        .current_dir(path_to_node_tests)
         .spawn()
         .unwrap_or_else(|_| panic!("Could not run command {:?}", cmd))
         .wait()
