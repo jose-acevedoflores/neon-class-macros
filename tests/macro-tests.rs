6161ba53c11d4c648e3477d48ad19052db5e1d25
Jose Acevedo <acevedoflores.jose@gmail.com>
2021-11-19 21:08:50 -0800

    test: fold node tests into cargo tests so they get all run by `cargo test`

--- tests/macro-tests.rs
+++ tests/macro-tests.rs
@@ -1,5 +1,22 @@
+use std::path::PathBuf;
+use std::process::Command;
+
+mod utils;
+
 #[test]
 fn try_build() {
     let t = trybuild::TestCases::new();
     t.pass("derived_class/src/derived_class.rs");
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+    let n = manifest_dir.join("derived_class");
+    let npm_path = utils::locate_npm();
+    let mut cmd = Command::new(npm_path);
+
+    cmd.arg("test")
+        .current_dir(n)
+        .spawn()
+        .unwrap_or_else(|_| panic!("Could not run command {:?}", cmd))
+        .wait()
+        .unwrap_or_else(|_| panic!("Could not run command {:?}", cmd));
 }
