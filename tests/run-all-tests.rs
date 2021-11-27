650222da38917e481916ac331c49897480ba898c
Jose Acevedo <acevedoflores.jose@gmail.com>
2021-11-27 15:56:59 -0800

    test: fix tests so that failure on the child procs fails the test

--- tests/run-all-tests.rs
+++ tests/run-all-tests.rs
@@ -30,10 +30,14 @@ fn run_cmd(cmd_path: PathBuf) {
 
     let mut cmd = Command::new(cmd_path);
 
-    cmd.args(["test"])
+    let res = cmd
+        .args(["test"])
         .current_dir(path_to_node_tests)
         .spawn()
         .unwrap_or_else(|_| panic!("Could not spawn command {:?}", cmd))
         .wait()
-        .unwrap_or_else(|_| panic!("Could not wait command {:?}", cmd));
+        .unwrap_or_else(|_| panic!("Could not wait command {:?}", cmd))
+        .success();
+
+    assert!(res);
 }
