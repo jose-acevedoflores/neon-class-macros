3d6374030d06659cb6cb7a863cc0589a6c95e6d3
Jose Acevedo <acevedoflores.jose@gmail.com>
2021-11-26 23:57:12 -0800

    test: clean up test runner and add warning if tests are ran without the 'for-tests' feature

--- tests/run-all-tests.rs
+++ tests/run-all-tests.rs
@@ -3,30 +3,32 @@ use std::process::Command;
 
 mod utils;
 
+#[cfg(feature = "for-tests")]
+fn check_feature() {}
+
+#[cfg(not(feature = "for-tests"))]
+fn check_feature() {
+    panic!("Running tests without the 'for-tests' feature will result in errors. Use the alias `cargo t`");
+}
+
 #[test]
 fn node_tests() {
-    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-
-    let path_to_node_tests = manifest_dir.join("node_tests");
-    let npm_path = utils::locate_npm();
-    let mut cmd = Command::new(npm_path);
-
-    cmd.arg("test")
-        .current_dir(path_to_node_tests)
-        .spawn()
-        .unwrap_or_else(|_| panic!("Could not spawn command {:?}", cmd))
-        .wait()
-        .unwrap_or_else(|_| panic!("Could not wait command {:?}", cmd));
+    let npm_cmd = utils::locate_npm();
+    run_cmd(npm_cmd);
 }
 
 #[test]
 fn try_build_tests() {
-    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let cargo_cmd = PathBuf::from(env!("CARGO"));
+    run_cmd(cargo_cmd);
+}
 
+fn run_cmd(cmd_path: PathBuf) {
+    check_feature();
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let path_to_node_tests = manifest_dir.join("node_tests");
 
-    let mut cmd = Command::new(cargo_cmd);
+    let mut cmd = Command::new(cmd_path);
 
     cmd.args(["test"])
         .current_dir(path_to_node_tests)
